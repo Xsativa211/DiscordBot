2726d8aaabcd339f02819d5968594ddf7c1ef02b
Xsativa211 <47264075+Xsativa211@users.noreply.github.com>
2019-02-10 23:20:18 +0800

    Create tutorialbot.py

---
+++ tutorialbot.py
@@ -0,0 +1,37 @@
+import discord
+from discord.ext.commands import Bot
+from discord.ext import commands
+import asyncio
+import time
+import random
+from discord import Game
+
+
+Client = discord.client
+client = commands.Bot(command_prefix = '!')
+Clientdiscord = discord.Client()
+
+
+@client.event
+async def on_ready():
+    await client.change_presence(game=Game(name='My Own Feelings'))
+    print('Working na po') 
+
+
+@client.event
+async def on_message(message):
+    if message.content == 'g!':
+        await client.send_message(message.channel,'Syempre ahh!')
+
+@client.event
+async def on_message(message):
+    if message.content == '!g!2':
+        await client.send_message(message.channel,'@forallpeople03#1592, Hello sa iyo!')
+
+@client.event
+async def on_message(message):
+    if message.content == '!g!3':
+        await client.send_message(message.channel,'tol, I don’t know what the FUCK you did. Sumisigaw yung anak ko sa taas, “Daddy! Daddy! Daddy!” Sabi ko, “Anak, bakit?” HAH? “Si tito Pker hindi ko alam nakatayo nalang jan hawak hawak yung kamay ko."')
+        
+        
+client.run(str(os.environ.get('TOKEN')))
