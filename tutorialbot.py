24a861d1c1c36d44f2576b96a87484b1a0e3e3ec
Xsativa211 <47264075+Xsativa211@users.noreply.github.com>
2019-02-11 14:16:53 +0800

    Update tutorialbot.py

--- tutorialbot.py
+++ tutorialbot.py
@@ -15,11 +15,6 @@ Clientdiscord = discord.Client()
 
 
 @client.event
-async def on_ready():
-    await client.change_presence(game=Game(name='Ragnarok Online Mobile'))
-    print('Working na po') 
-    
-@client.event
 async def on_message(message):
     if message.content == 'levie':
         await client.send_message(message.channel,'tol, I don’t know what the FUCK you did. Sumisigaw yung anak ko sa taas, “Daddy! Daddy! Daddy!” Sabi ko, “Anak, bakit?” HAH? “Si tito Pker hindi ko alam nakatayo nalang jan hawak hawak yung kamay ko."')
@@ -29,13 +24,21 @@ async def on_message(message):
        await client.send_message(message.channel,'tol, I don’t know what the FUCK you did. Sumisigaw yung anak ko sa taas, “Daddy! Daddy! Daddy!” Sabi ko, “Anak, bakit?” HAH? “Si tito Pker hindi ko alam nakatayo nalang jan hawak hawak yung kamay ko."')
     if ('Levie') in message.content:
        await client.delete_message(message)
-    if message.content == '!Invite':
+    if message.content == '!link':
        await client.send_message(message.channel,'Discord Invitation link\nhttps://discord.me/genocidero')
-    if ('!Invite') in message.content:
+    if ('!link') in message.content:
        await client.delete_message(message)
-    if message.content == '!invite':
+    if message.content == '!Link':
        await client.send_message(message.channel,'Discord Invitation link \nhttps://discord.me/genocidero')
-    if ('!invite') in message.content:
+    if ('!Link') in message.content:
+       await client.delete_message(message)    
+    if message.content == '!rules':
+       await client.send_message(message.channel,':regional_indicator_r: :regional_indicator_u: :regional_indicator_l: :regional_indicator_e: :regional_indicator_s: \n1: respect OFFICERS and CoMembers\n\n2: bawal Mahiyain.\n\n3: GENDER EQUALITY lalo na may LGBT tayo dito sa guild.\n\n4: bawal din sapilitan nang hihingi ng tulong wait nyo maubos ang CT nila tutulungan din naman nila kayo.\n\n5: Wag Tawagin sa hindi panglan ang isang CoMember.\n\n6: kung meron ka isasali sa guild pasalihin mo muna sa DISCORD.\n\n7: bawal ang mayabang :)\n\n8: Share Knowledge ( dahil hindi ka naman mauubusan nyan ) tulungan mo ung iba.\n\n9: Always Check DISCORD for news and events.\n\n10: Wag ka masyado mag ADIK may BUHAY kapa sa LABAS ng RAGNAROK,. And Mag Enjoy lang tayo ')
+    if ('!rules') in message.content:
+       await client.delete_message(message)  
+    if message.content == '!Rules':
+       await client.send_message(message.channel,':regional_indicator_r: :regional_indicator_u: :regional_indicator_l: :regional_indicator_e: :regional_indicator_s: \n1: respect OFFICERS and CoMembers\n\n2: bawal Mahiyain.\n\n3: GENDER EQUALITY lalo na may LGBT tayo dito sa guild.\n\n4: bawal din sapilitan nang hihingi ng tulong wait nyo maubos ang CT nila tutulungan din naman nila kayo.\n\n5: Wag Tawagin sa hindi panglan ang isang CoMember.\n\n6: kung meron ka isasali sa guild pasalihin mo muna sa DISCORD.\n\n7: bawal ang mayabang :)\n\n8: Share Knowledge ( dahil hindi ka naman mauubusan nyan ) tulungan mo ung iba.\n\n9: Always Check DISCORD for news and events.\n\n10: Wag ka masyado mag ADIK may BUHAY kapa sa LABAS ng RAGNAROK,. And Mag Enjoy lang tayo ')
+    if ('!Rules') in message.content:
        await client.delete_message(message)    
     
     
