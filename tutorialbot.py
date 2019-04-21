dd1f96d5b4eebf13b218fae2734e52c777829434
Xsativa211 <47264075+Xsativa211@users.noreply.github.com>
2019-04-21 12:50:48 +0800

    Update tutorialbot.py

--- tutorialbot.py
+++ tutorialbot.py
@@ -16,7 +16,6 @@ Clientdiscord = discord.Client()
 @client.event
 async def on_ready():
     await client.change_presence(game=Game(name='Ragnarok Online Mobile',))
-    status=discord.Status("dnd")
     print('Codes are working perfectly fine! you may use it now!') 
 
 @client.event
